fdba687f40f30c03bbcedd03d85b0a41bf67d767
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-21 23:50:57 -0300

    feat: create hook authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,12 @@
-import React, { createContext, useState, useEffect } from 'react';
 import Routers from "./components/Routers";
-import { auth, firebase } from './services/firebase';
 
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        const { displayName, photoURL, uid } = user;
-      
-        if(!displayName || !photoURL) {
-          throw new Error("Missing information from google Account.");
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL,
-        });
-      }
-    });
-  }, []);
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if(result.user) {
-      const { displayName, photoURL, uid } = result.user;
-      
-      if(!displayName || !photoURL) {
-        throw new Error("Missing information from google Account.");
-    }
-
-    setUser({
-      id: uid,
-      name: displayName,
-      avatar: photoURL,
-    });
-
-    }
-  }
-
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+    <AuthContextProvider>
       <Routers />
-    </AuthContext.Provider>
+    </AuthContextProvider>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,12 +5,12 @@ import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext)
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if(!user) {
