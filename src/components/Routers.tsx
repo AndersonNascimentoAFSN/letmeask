402d84dc19139e3d2e594e4b9984ad986b48572a
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-22 21:07:32 -0300

    feat: create class

--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from "react-router";
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
+import { Room } from '../pages/Room';
 
 
 export default function Routers() {
   return (
     <Switch>
-      <Route path="/rooms/new" component={NewRoom}/>
+      <Route exact path="/rooms/new" component={NewRoom}/>
+      <Route path="/rooms/:id" component={Room}/>
       <Route exact={true} path="/" component={Home}/>
     </Switch>
   )
