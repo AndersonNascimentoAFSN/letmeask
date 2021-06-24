222bd70121abefb435ac35fd3009d38f84382300
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-23 22:35:19 -0300

    feat: create  admin class

--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from "react-router";
+import { AdminRoom } from '../pages/AdminRoom';
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
 import { Room } from '../pages/Room';
@@ -9,6 +10,7 @@ export default function Routers() {
   return (
     <Switch>
       <Route exact path="/rooms/new" component={NewRoom}/>
+      <Route exact path="/admin/rooms/:id" component={AdminRoom} />
       <Route path="/rooms/:id" component={Room}/>
       <Route exact={true} path="/" component={Home}/>
     </Switch>
